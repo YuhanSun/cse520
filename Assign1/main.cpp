210beab151bae2f703b4b0b0ebc51137a09c49b7
yuhansun <wdmzssyh@gmail.com>
2019-01-29 23:01:17 -0700

    update random read

--- Assign1/main.cpp
+++ Assign1/main.cpp
@@ -5,23 +5,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctime>
+#include <algorithm>
 
 using namespace std;
 
-int main(int argc, char * argv[]) {
-    int array_size = atoi(argv[1]);
-    cout << array_size << ',';
-//    int testCount = 100000; //how many times for testing, will be averaged by this
-
-//    int loopCount = testCount / array_size; //read the array for loopCount
+void seq_read(int a[], int array_size) {
+    // sequential
     int loopCount = 10000;
     long total_ns = 0;
     long avg_ns = 0;
     int val = 0;
 
-    // sequential
-//    int * a = new int[array_size];
-    int a[100000];
 //    if (loopCount == 0) {
 //        int access_count = 0;
 //        for (int i = 0; i < array_size; i++) {
@@ -40,10 +35,10 @@ int main(int argc, char * argv[]) {
 //    auto start = chrono::steady_clock::now();
     clock_t start = clock();
     for (int loopIndex = 0; loopIndex < loopCount; loopIndex++) {
-            for (int i = 0; i < array_size; i++) {
+        for (int i = 0; i < array_size; i++) {
 //                cout << a[i] << endl;
-                val += a[i];
-            }
+            val += a[i];
+        }
     }
     clock_t end = clock();
 //    auto end = chrono::steady_clock::now();
@@ -53,6 +48,59 @@ int main(int argc, char * argv[]) {
 //    }
     cout << (avg_ns) << endl;
     val = val+1;
+}
+
+void random_read(int a[], int array_size) {
+    int loopCount = 10000;
+    long total_ns = 0;
+    long avg_ns = 0;
+    int val = 0;
+    srand(time(NULL));
+    vector<int> v(array_size);
+    for (int i = 0; i < array_size; i++) {
+        v[i] = i;
+    }
+    random_shuffle(v.begin(), v.end());
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == i && i + 1 < v.size()) {
+            int temp = v[i];
+            v[i] = v[i+1];
+            v[i+1] = temp;
+        }
+        a[i] = v[i];
+    }
+
+    clock_t start = clock();
+    for (int loopIndex = 0; loopIndex < loopCount; loopIndex++) {
+        int idx = 0;
+        for (int i = 0; i < array_size; i++) {
+//            val += a[i];
+            idx = a[idx];
+//            cout << idx << endl;
+        }
+    }
+    clock_t end = clock();
+    total_ns += (double) (end - start) / CLOCKS_PER_SEC * 1000000000;
+//    auto end = chrono::steady_clock::now();
+//    total_ns += chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+
+    avg_ns = total_ns / loopCount / array_size;
+//    }
+    cout << "avg: " << (avg_ns) << endl;
+    val = val+1;
+}
+
+int main(int argc, char * argv[]) {
+    int array_size = atoi(argv[1]);
+    cout << array_size << ',';
+//    int testCount = 100000; //how many times for testing, will be averaged by this
+
+//    int loopCount = testCount / array_size; //read the array for loopCount
+//    int * a = new int[array_size];
+    int a[1000000];
+
+//    seq_read(a, array_size);
+    random_read(a, array_size);
 
 //    cout << argc << endl;
 //    vector<int> array;
