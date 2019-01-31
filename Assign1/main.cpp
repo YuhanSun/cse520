406c5286b3bc287e408ce23dc8ef5b2864b659cb
yuhansun <wdmzssyh@gmail.com>
2019-01-30 23:50:54 -0700

    add comment

--- Assign1/main.cpp
+++ Assign1/main.cpp
@@ -10,8 +10,8 @@
 using namespace std;
 
 volatile int val = 0;
-int a[2000000];
-int test_count = 100000;
+int a[2000000];// the test array
+int test_count = 100000;// how many times for read
 
 /**
  * Read array in a sequential way
@@ -34,8 +34,8 @@ void seq_read(int a[], int array_size, int loop_count) {
         }
     }
     clock_t end = clock();
-    total_ns = (double) (end - start) / CLOCKS_PER_SEC * 1000000000;
-    avg_ns = total_ns / loop_count / array_size;
+    total_ns = (double) (end - start) / CLOCKS_PER_SEC * 1000000000;// the total ns
+    avg_ns = total_ns / loop_count / array_size;// average access time
     cout << (avg_ns) << endl;
 }
 
@@ -54,15 +54,17 @@ void random_read(int a[], int array_size, int loop_count) {
         v[i] = i;
     }
 
-    // Generate the chasing pointer
+    // Generate the chasing pointer.
+    // This can make sure each element is accessed exactly once.
     random_shuffle(v.begin(), v.end());
     for (int i = 0; i < array_size; i++) {
+        // make sure v[i] != i
         if (v[i] == i && i + 1 < array_size) {
             int temp = v[i];
             v[i] = v[i+1];
             v[i+1] = temp;
         }
-        a[i] = v[i];
+        a[i] = v[i];//initialize the a[i] with random value
     }
 
     // Record the random read time of an array
@@ -70,7 +72,7 @@ void random_read(int a[], int array_size, int loop_count) {
     for (int loopIndex = 0; loopIndex < loop_count; loopIndex++) {
         int idx = 0;
         for (int i = 0; i < array_size; i++) {
-            idx = a[idx];
+            idx = a[idx];//chase pointer to next position
             val += a[idx];
         }
     }
@@ -92,7 +94,7 @@ void seq_write(int a[], int array_size, int loop_count) {
     clock_t start = clock();
     for (int loop_index = 0; loop_index < loop_count; loop_index++) {
         for ( int i = 0; i < array_size; i++) {
-            a[i] = i;
+            a[i] = i; // linear write
         }
     }
     clock_t end = clock();
@@ -119,6 +121,7 @@ void random_write(int a[], int array_size, int loop_count) {
     // Generate the chasing pointer
     random_shuffle(v.begin(), v.end());
 
+    // same with random read, now v stores the random value
     for (int i = 0; i < array_size; i++) {
         if (v[i] == i && i + 1 < array_size) {
             int temp = v[i];
@@ -127,12 +130,12 @@ void random_write(int a[], int array_size, int loop_count) {
         }
     }
 
-    // Record the random read time of an array
+    // Record the random write time of an array
     clock_t start = clock();
     for (int loopIndex = 0; loopIndex < loop_count; loopIndex++) {
         for (int i = 0; i < array_size; i++) {
-            int idx = v[i];
-            a[idx] = i;
+            int idx = v[i]; // the random position
+            a[idx] = i; // write to a random position
             val += a[idx];
         }
     }
@@ -143,14 +146,24 @@ void random_write(int a[], int array_size, int loop_count) {
 }
 
 int main(int argc, char * argv[]) {
+    // the operations to be tested:
+    // 0: linear read
+    // 1: random read
+    // 2: linear write
+    // 3: random write
     int op = atoi(argv[1]);
+
+    // the array size
     int array_size = atoi(argv[2]);
     cout << array_size << ',';
 
+    // how many times to access the same array and get the average
     int loop_count = test_count / array_size;
+    // if the array_size is larger than the test_count, at least traverse the array from 0 to array_size once
     if (loop_count == 0) {
         loop_count = 1;
     }
+
     switch (op){
         case 0:
             seq_read(a, array_size, loop_count);
--- Assign1/script/task2.sh
+++ Assign1/script/task2.sh
@@ -5,10 +5,12 @@ script_dir="/home/user/Documents/520/program/cse520/Assign1/script"
 
 cd ${gem5dir}
 
-#for i in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576; do
-#	${gem5dir}/build/X86/gem5.opt ${gem5dir}/configs/example/se.py -c ${script_dir}/main '--options=0 '${i}''
-#	mv ${gem5dir}/m5out/stats.txt ${script_dir}/gem5_seq_read_${i}.txt
-#done
+for i in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576; do
+	# generate stat.txt for each operation and array_size
+	${gem5dir}/build/X86/gem5.opt ${gem5dir}/configs/example/se.py -c ${script_dir}/main '--options=0 '${i}''
+	# move the stats.txt to the script directory
+	mv ${gem5dir}/m5out/stats.txt ${script_dir}/gem5_seq_read_${i}.txt
+done
 
 for i in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576; do
 	${gem5dir}/build/X86/gem5.opt ${gem5dir}/configs/example/se.py -c ${script_dir}/main '--options=1 '${i}''
